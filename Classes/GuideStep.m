e397f5d8cda83930d43647d45c4cd058a2a65f42
Stefan Ayala <stefan@ifixit.com>
2012-12-18 15:36:26 -0800

    Fix iOS App from crashing when a guidestep has no image
    When there was an empty image on a guide step, it would crash the app because
    it was looking for the key "type" in the dictionary "media". All guide steps
    will have a "media" key, but not a "type" key. Apparently this is changing in
    API 1.1 but for now this will do.
    
    What happened before: Tried to access key "type" within the media dictionary
    and app would crash.
    
    What happens now: If the key type doesn't exist, lets default to "image" type.
    We now also add a background image to the Main Image which is the Ruler image.
    If images are found they will be placed as a foreground image. Otherwise
    nothing will happen and the Ruler image will display.

--- Classes/GuideStep.m
+++ Classes/GuideStep.m
@@ -19,25 +19,26 @@
 	guideStep.number = [[dict valueForKey:@"number"] integerValue];
 	guideStep.title = [dict valueForKey:@"title"];
 	
-  // Media
-  NSDictionary *media = [dict valueForKey:@"media"];
+    // Media
+    NSDictionary *media = [dict valueForKey:@"media"];
 
-  // Possible types: image, video, embed
-  NSString *type = [media objectForKey:@"type"];
-  if ([type isEqual:@"image"]) {
-    guideStep.images = [NSMutableArray array];
-    NSArray *images = [media valueForKey:@"image"];
-    for (NSDictionary *image in images)
-      [guideStep.images addObject:[GuideImage guideImageWithDictionary:image]];    
-  }
-  else if ([type isEqual:@"video"]) {
-    NSDictionary *video = [media objectForKey:@"video"];
-    guideStep.video = [GuideVideo guideVideoWithDictionary:video];
-  }
-  else if ([type isEqual:@"embed"]) {
-    NSDictionary *embed = [media objectForKey:@"embed"];
-    guideStep.embed = [GuideEmbed guideEmbedWithDictionary:embed];
-  }
+    // Possible types: image, video, embed
+    // If *media is empty, then there will be no
+    // type so we want to default to an image type
+    NSString *type = (media.count > 0) ? [media objectForKey:@"type"] : @"image";
+
+    if ([type isEqual:@"image"]) {
+        guideStep.images = [NSMutableArray array];
+        NSArray *images = [media valueForKey:@"image"];
+        for (NSDictionary *image in images)
+            [guideStep.images addObject:[GuideImage guideImageWithDictionary:image]];
+    } else if ([type isEqual:@"video"]) {
+        NSDictionary *video = [media objectForKey:@"video"];
+        guideStep.video = [GuideVideo guideVideoWithDictionary:video];
+    } else if ([type isEqual:@"embed"]) {
+        NSDictionary *embed = [media objectForKey:@"embed"];
+        guideStep.embed = [GuideEmbed guideEmbedWithDictionary:embed];
+    }
 
 	// Lines
 	guideStep.lines = [NSMutableArray array];
