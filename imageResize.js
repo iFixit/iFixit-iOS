a8b0ce83bfafd8b575b788121da6d99cfccac80f
BaseInfinity <stefanayala3266@gmail.com>
2013-11-25 21:19:40 -0800

    Add extension property

--- imageResize.js
+++ imageResize.js
@@ -1,39 +1,20 @@
 var fs = require('fs'),
     gm = require('gm'),
-    baseIconPath = '', // Path for base icon image
-    baseIconImage = gm(baseIconPath),
-    // iOS resource list
-    resourceList = [
-       // App icons
-       { name : 'icon57.png', dimensions  : { width : 57, height : 57 } },
-       { name : 'icon114.png', dimensions : { width : 114, height : 114 } },
-       { name : 'icon120.png', dimensions : { width : 120, height : 120 } },
-       { name : 'icon72.png', dimensions  : { width : 72, height : 72 } },
-       { name : 'icon144.png', dimensions : { width : 144, height : 144 } },
-       { name : 'icon76.png', dimensions  : { width : 76, height : 76 } },
-       { name : 'icon152.png', dimensions : { width : 152, height : 152 } },
-       // Spotlight
-       { name : 'icon29.png', dimensions  : { width : 29, height : 29 } },
-       { name : 'icon58.png', dimensions  : { width : 58, height : 58 } },
-       { name : 'icon80.png', dimensions  : { width : 80, height : 80 } },
-       { name : 'icon50.png', dimensions  : { width : 50, height : 50 } },
-       { name : 'icon100.png', dimensions : { width : 100, height : 100 } },
-       { name : 'icon40.png', dimensions  : { width : 40, height : 40 } }
-   ];
+    resources = require('AppIconResources.js').resources,
+    baseIconPath = 'icon1024.png', // Path for base icon image
+    baseIconImage = gm(baseIconPath);
 
 // Generate image sizes
-for (var i = 0; i < resourceList.length; i++) {
-   var resource = resourceList[i];
+for (var i = 0; i < resources.length; i++) {
+   var resource = resources[i];
 
-   baseIconImage
-   .resize(resource.dimensions.width, resource.dimensions.height)
-   .noProfile()
-   .write(resource.name, function(err) {
+   baseIconImage.resize(resource.dimensions.width, resource.dimensions.height)
+   .noProfile().write((resource.name + '.' + resource.extension), function(err) {
       if (err) {
-         console.log('error: ' + err);
-         return;
+         throw new Error(err);
+      } else {
+         console.log('Image generated successfully');
       }
    });
 }
 
-console.log('Images generated successfully');
