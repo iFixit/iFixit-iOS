6918a4fc1f655a343b82ce4bc596518b90d975a8
BaseInfinity <stefanayala3266@gmail.com>
2013-11-26 08:58:15 -0800

    Use JSON instead of a module

--- imageResize.js
+++ imageResize.js
@@ -1,15 +1,23 @@
 var fs = require('fs'),
     gm = require('gm'),
-    resources = require('AppIconResources.js').resources,
     baseIconPath = 'icon1024.png', // Path for base icon image
-    baseIconImage = gm(baseIconPath);
+    baseIconImage = gm(baseIconPath),
+    resourcePath = 'AppIconResources.json'; // Path where resource info is
+
+// Load up the resource list we wish to make from JSON
+var resources = JSON.parse(require('fs').readFileSync(resourcePath,
+ 'utf8'));
+
+if (!resources) {
+   throw new Error('json file not found');
+}
 
 // Generate image sizes
 for (var i = 0; i < resources.length; i++) {
    var resource = resources[i];
 
    baseIconImage.resize(resource.dimensions.width, resource.dimensions.height)
-   .noProfile().write((resource.name + '.' + resource.extension), function(err) {
+   .noProfile().write(resource.name + '.' +  resource.extension, function(err) {
       if (err) {
          throw new Error(err);
       } else {
