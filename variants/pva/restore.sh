4bc727b4bec96bc7f788ac5c6d4be2c03860d967
Bob Pascazio <bob@managedapps.co>
2018-11-04 15:59:01 +0700

    make updates for pva compilation

---
+++ variants/pva/restore.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo restoring dozuki
+
+git checkout -- ../../icon*
+git checkout -- ../../Default*
+git checkout -- ../../iFixit
+git checkout -- ../../iFixit.xcworkspace
+git checkout -- ../../iFixit.xcodeproj
+git checkout -- ../../iFixit-Info.plist
+git checkout -- ../../dozuki.sh
+git checkout -- ../../Classes/iFixitAppDelegate.m
+git checkout -- ../../Classes/iFixitAppDelegate.m
+git checkout -- ../../Classes/Config.m
+git checkout -- ../../Classes/iFixitAppDelegate.m
+git checkout -- ../../Classes/iFixitAppDelegate.m
+git checkout -- ../../Classes/Config.m
+git checkout -- ../../Classes/CategoriesViewController.m
+git checkout -- ../../Classes/Config.h
+git checkout -- ../../Classes/iFixitAPI.m
