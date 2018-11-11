b2af930ee7d7a42fc4e1e1afb27e225ff6cf78a8
Bob Pascazio <bob@managedapps.co>
2018-11-11 10:26:37 +0700

    simplify aristo setup

--- variants/aristo/install.sh
+++ variants/aristo/install.sh
@@ -2,17 +2,23 @@
 
 echo making aristo
 
+# images
 cp -v titleImageAristocrat.png ../../titleImageAristocrat.png
 cp -v titleImagePepsi.png ../../titleImagePepsi.png
-cp -v iFixit-Info.plist ../../iFixit-Info.plist
 cp -v Default* ../../
 mkdir ../../iFixit/Images.xcassets/AppIcon-3.appiconset
 cp -v AppIcon-3.appiconset/* ../../iFixit/Images.xcassets/AppIcon-3.appiconset
-cp -R Aristocrat.xcodeproj ../../Aristocrat.xcodeproj
-cp -R Aristocrat.xcworkspace ../../Aristocrat.xcworkspace
+
+# config code
 cp -v Config.m ../../Classes/Config.m
 cp -v iFixitAppDelegate.m ../../Classes/iFixitAppDelegate.m
+
+# plist
+cp -v iFixit-Info.plist ../../iFixit-Info.plist
+
+#extra
 cp -v Podfile ../../Podfile
 cp -v Podfile.lock ../../Podfile.lock
 rm -rf ../../Pods
 cp -R Pods ../../Pods
+
--- variants/aristo/restore.sh
+++ variants/aristo/restore.sh
@@ -2,6 +2,7 @@
 
 echo restoring dozuki
 
+# image restore
 git checkout -- ../../Default*
 git checkout -- ../../Default-568h@2x.png
 git checkout -- ../../Default-Landscape.png
@@ -12,7 +13,6 @@ git checkout -- ../../Default-Portrait@2x~ipad.png
 git checkout -- ../../Default-Portrait~ipad.png
 git checkout -- ../../Default.png
 git checkout -- ../../Default@2x.png
-git checkout -- ../../Podfile.lock
 rm ../../Default-667h@2x.png
 rm ../../Default-Landscape-736h@3x.png
 rm ../../Default-Portrait-736h@3x.png
@@ -21,12 +21,15 @@ rm ../../Default-Portrait@2x.png
 rm ../../iFixit/Images.xcassets/AppIcon-3.appiconset/Icon-App-Aristo-1024x1024.png
 rm ../../titleImageAristocrat.png
 rm ../../titleImagePepsi.png
-git checkout -- ../../iFixit-Info.plist
 git checkout -- ../../iFixit/Images.xcassets/AppIcon-3.appiconset
-rm -rf ../../Aristocrat.xcodeproj
-rm -rf ../../Aristocrat.xcworkspace
+
+# configuration files
+git checkout -- ../../iFixit-Info.plist
 git checkout -- ../../Classes/Config.m
 git checkout -- ../../Classes/iFixitAppDelegate.m
+
+#extra
 git checkout -- ../../Podfile
+git checkout -- ../../Podfile.lock
 rm -rf ../../Pods
 git checkout -- ../../Pods
